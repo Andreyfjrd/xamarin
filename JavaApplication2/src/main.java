cb3169af3f206ca03ec36bdb088e5930fb42fb1d
andrey <andrey@172.16.1.121>
2017-11-02 10:02:19 -0600

    Se agrega main

---
+++ JavaApplication2/src/main.java
@@ -0,0 +1,18 @@
+
+
+/**
+ *
+ * @author andrey
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        
+        //Agruegue alguna linea de prueba. 
+    }
+    
+}
