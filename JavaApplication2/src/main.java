d66e35d4bcac7193535ccb960baa5230480db115
andrey <andrey@172.16.1.121>
2017-11-02 12:52:23 -0600

    Pruebas de cambios

--- JavaApplication2/src/main.java
+++ JavaApplication2/src/main.java
@@ -12,7 +12,11 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hola Andrey");
         
-        //Texto de prueba. 
+        //Texto de prueba.
+        
+        System.out.println("Por favor no borrar lo que pongo jaja");
+        
+        
     }
     
 }
